18720047a8ad2148e5c6c0182aa61028fb1333f4
stroker351w <130164419+stroker351w@users.noreply.github.com>
2023-04-10 10:26:27 -0400

    Update ConnectButton.js

--- neovest/src/components/ConnectButton.js
+++ neovest/src/components/ConnectButton.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 
 const ConnectButton = () => {
   const handleClick = async () => {
-    const clientId = 'your_client_id_here';
-    const clientSecret = 'your_client_secret_here';
+    const clientId = 'b01f434d-5ce9-40fd-3eab-08db08692dca';
+    const clientSecret = 'a76acc09caf54c648cf4bbcad5c3dacd';
 
     try {
       const response = await axios.get('https://dashboard.getfront.com/api/v1/cataloglink', {
